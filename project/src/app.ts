493404ebef6e9d72257f207bacfcf7c701171699
Bartek Karelus <bartek.karelus@gmail.com>
2021-01-13 21:55:26 +0100

    add submithandler, get few ElementsBy ID

--- project/src/app.ts
+++ project/src/app.ts
@@ -2,19 +2,34 @@ class ProjectManager {
     templateElement: HTMLTemplateElement;
     appElement: HTMLDivElement;
     formElement: HTMLFormElement;
+    inputTitle: HTMLInputElement;
+    inputDescription: HTMLInputElement;
+    inputNumber: HTMLInputElement;
 
     constructor() {
-        console.log("2")
         this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
         this.appElement = document.getElementById("app")! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.formElement = importedNode.firstElementChild as HTMLFormElement;
+        this.formElement.id = "user-input"
+        this.submitButton()
         this.attach()
+        this.inputTitle = document.getElementById("title") as HTMLInputElement;
+        this.inputDescription = document.getElementById("description") as HTMLInputElement;
+        this.inputNumber = document.getElementById("people") as HTMLInputElement;
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.inputTitle.value)
     }
 
     private attach() {
         this.appElement.insertAdjacentElement("afterbegin", this.formElement)
+    }
+    private submitButton() {
+        this.formElement.addEventListener("submit", this.submitHandler.bind(this))
     }
 }
 
